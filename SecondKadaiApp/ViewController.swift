096f62c64d701f43c19e50cf88f27c198b2d1527
maaya.maruyama <m.maaya@icloud.com>
2023-10-24 21:28:36 +0900

    挨拶アプリを作成した．

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.delegate = self
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.name = textField.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
     }
 
 
